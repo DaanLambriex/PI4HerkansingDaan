bd9ba6685a7fa419b9094d875e69ab266a633d98
DaanLambriex <146425574+DaanLambriex@users.noreply.github.com>
2024-12-05 14:54:49 +0100

    Database toegevoegd

---
+++ Collectible.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PI4Daan
+{
+    public class Collectible
+    {
+        public int Id { get; set; }  //Primary Key
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0, 999999.99)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [Range(0, 999999.99)]
+        [DataType(DataType.Currency)]
+        public decimal Value { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        [Range(0, 100.0)]
+        public decimal Percentage { get; set; }
+        [Range(0, 5.0)]
+        public double Rating { get; set; }
+    }
+}
---
+++ Controllers/CollectiblesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PI4Daan;
+using PI4Daan.Data;
+
+namespace PI4Daan.Controllers
+{
+    public class CollectiblesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CollectiblesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Collectibles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Collectibles.ToListAsync());
+        }
+
+        // GET: Collectibles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collectible = await _context.Collectibles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (collectible == null)
+            {
+                return NotFound();
+            }
+
+            return View(collectible);
+        }
+
+        // GET: Collectibles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Collectibles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,Value,Category,Brand,Percentage,Rating")] Collectible collectible)
+        {
+            if (ModelState.IsValid)
+            {
+                // Valideer de prijs en waarde op correct formaat
+                collectible.Price = Math.Round(collectible.Price, 2);
+                collectible.Value = Math.Round(collectible.Value, 2);
+                collectible.Percentage = Math.Round(collectible.Percentage, 1);
+
+                _context.Add(collectible);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(collectible);
+        }
+
+        // GET: Collectibles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collectible = await _context.Collectibles.FindAsync(id);
+            if (collectible == null)
+            {
+                return NotFound();
+            }
+            return View(collectible);
+        }
+
+        // POST: Collectibles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Value,Category,Brand,Percentage,Rating")] Collectible collectible)
+        {
+            if (id != collectible.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                collectible.Percentage = Math.Round(collectible.Percentage, 1);
+
+                try
+                {
+                    _context.Update(collectible);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CollectibleExists(collectible.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(collectible);
+        }
+
+        // GET: Collectibles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collectible = await _context.Collectibles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (collectible == null)
+            {
+                return NotFound();
+            }
+
+            return View(collectible);
+        }
+
+        // POST: Collectibles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var collectible = await _context.Collectibles.FindAsync(id);
+            if (collectible != null)
+            {
+                _context.Collectibles.Remove(collectible);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CollectibleExists(int id)
+        {
+            return _context.Collectibles.Any(e => e.Id == id);
+        }
+    }
+}
